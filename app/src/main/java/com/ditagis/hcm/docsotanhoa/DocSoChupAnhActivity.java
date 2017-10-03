9f6411c6c505ea249280d54bc8eabf7ceaf583e9
thanle95 <lengocthan95@gmail.com>
2017-10-03 14:43:33 +0700

    layout login

---
+++ app/src/main/java/com/ditagis/hcm/docsotanhoa/DocSoChupAnhActivity.java
@@ -0,0 +1,13 @@
+package com.ditagis.hcm.docsotanhoa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocSoChupAnhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_so_chup_anh);
+    }
+}
