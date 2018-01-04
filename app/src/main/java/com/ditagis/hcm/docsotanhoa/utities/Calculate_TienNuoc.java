e713939cf836096fdc3c9a3392ba079c9409ab54
thanle95 <lengocthan95@gmail.com>
2018-01-04 07:15:55 +0700

    calculate tiennuoc

--- app/src/main/java/com/ditagis/hcm/docsotanhoa/utities/Calculate_TienNuoc.java
+++ app/src/main/java/com/ditagis/hcm/docsotanhoa/utities/Calculate_TienNuoc.java
@@ -1,26 +1,23 @@
 package com.ditagis.hcm.docsotanhoa.utities;
 
-import com.ditagis.hcm.docsotanhoa.entities.Code_CSC_SanLuong;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by ThanLe on 11/22/2017.
  */
 
 public class Calculate_TienNuoc {
     private String mGB;
-    private Code_CSC_SanLuong mCodeCSCSanLuong;
-    private int mCSC, mCSM, mTieuThu, mTienNuoc, mBVMT, mVAT;
-    private String mCSMString;
+    private int mDM;
+    private int mTieuThu;
+    private double mTienNuoc, mBVMT, mVAT;
+    private int mSH, mSX, mDV, mHC;
+    private int SHTM, SHVM1, SHVM2;
 
-    public Calculate_TienNuoc(String code, Code_CSC_SanLuong mCodeCSCSanLuong, int csc, String csm) {
-        this.mGB = code;
-        this.mCodeCSCSanLuong = mCodeCSCSanLuong;
-        this.mCSMString = csm;
-        this.mCSC = csc;
-        this.mCSM = -1;
+    public Calculate_TienNuoc(String gb, int sh, int sx, int dv, int hc) {
+        this.mGB = gb;
+        mSH = sh;
+        mSX = sx;
+        mDV = dv;
+        mHC = hc;
         this.mTieuThu = -1;
         calculate();
     }
@@ -33,251 +30,255 @@ public class Calculate_TienNuoc {
         this.mGB = mGB;
     }
 
-    public Code_CSC_SanLuong getmCodeCSCSanLuong() {
-        return mCodeCSCSanLuong;
-    }
 
-    public void setmCodeCSCSanLuong(Code_CSC_SanLuong mCodeCSCSanLuong) {
-        this.mCodeCSCSanLuong = mCodeCSCSanLuong;
-    }
-
-    public int getmTienNuoc() {
+    public double getmTienNuoc() {
         return mTienNuoc;
     }
 
-    public int getmBVMT() {
+    public double getmBVMT() {
         return mBVMT;
     }
 
-    public int getmVAT() {
+    public double getmVAT() {
         return mVAT;
     }
 
-    public String getCSM() {
-
-        return mCSM == -1 ? "" : mCSM + "";
-    }
 
     public String getTieuThu() {
         return mTieuThu == -1 ? "" : mTieuThu + "";
+    }
+
+    private void reset() {
+        SHTM = SHVM1 = SHVM2 = 0;
+    }
+
+    private int getSHTM() {
+        return mTieuThu - mDM > 0 ? mDM : mTieuThu;
+    }
+
+    private int getSHVM() {
+        return mTieuThu - SHTM;
+    }
+
+    private int getSHVM1() {
+        return mTieuThu - 2 * mDM > 0 ? mDM : mTieuThu - SHTM;
+    }
+
+    private int getSHVM2() {
+        return mTieuThu - SHTM - SHVM1;
+    }
+
+    private boolean hasRatio() {
+        return mSH > 0 || mSX > 0 || mDV > 0 || mHC > 0;
+    }
+
+    private boolean hasSHVM() {
+        return SHTM >= mDM;
+    }
+
+    private boolean hasSHVM2() {
+        return SHVM1 >= mDM;
+    }
+
+    private void calculateSHTM_VM1_VM2() {
+        SHTM = getSHTM();
+        if (hasSHVM()) {
+            SHVM1 = getSHVM1();
+            if (hasSHVM2())
+                SHVM2 = getSHVM2();
+        }
+    }
+
+    private void calculateSHTM_VM() {
+        SHTM = getSHTM();
+        if (hasSHVM())
+            SHVM1 = getSHVM();
+    }
+
+    private int calculateTienNuoc_TM_VM1_VM2() {
+        return SHTM * 5300 + SHVM1 * 10200 + SHVM2 * 11400;
+    }
+
+    private int calculateSH() {
+        return mTieuThu * 11400;
+    }
+
+    private int calculateSX() {
+        return mTieuThu * 9600;
+    }
+
+    private int calculateDV() {
+        return mTieuThu * 16900;
+    }
+
+    private int calculateHC() {
+        return mTieuThu * 10300;
     }
 
     public void calculate() {
 
         switch (mGB) {
             case "11":
+                reset();
+                calculateSHTM_VM1_VM2();
+                mTienNuoc = calculateTienNuoc_TM_VM1_VM2();
+                break;
             case "12":
+            case "22":
+            case "32":
+            case "42":
+                mTienNuoc = mTieuThu * 9600;
+                break;
             case "13":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - mCSC;
-                }
-
+            case "23":
+            case "33":
+            case "43":
+                mTienNuoc = mTieuThu * 16900;
                 break;
             case "14":
-            case "15":
-            case "16":
-            case "17":
-            case "18":
-            case "19":
-            case "21":
-//                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-//                    mCSM = Integer.parseInt(mCSMString);
-//                else
-                mCSM = 0;
-                mTieuThu = 0;
-                break;
-            case "22":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - Integer.parseInt(mCodeCSCSanLuong.getSanLuong1()) - Integer.parseInt(mCodeCSCSanLuong.getCSC1());
-                }
-                //todo code 5F
-                break;
-            case "23":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - mCSC;
-                }
-                //todo code 5K
-                break;
             case "24":
-                //todo ghi chỉ số ngưng
-
-                mTieuThu = calTieuThuTB();
-                mCSM = -1;
+                reset();
+                if (hasRatio()) {
+                    calculateSHTM_VM1_VM2();
+                    double ratioSH = mSH / 100, ratioSX = mSX / 100;
+                    mTienNuoc = ratioSH * calculateTienNuoc_TM_VM1_VM2() + ratioSX * calculateSX();
+                } else {
+                    calculateSHTM_VM();
+                    mTienNuoc = SHTM * 5300 + SHVM1 * 9600;
+                }
                 break;
+            case "15":
             case "25":
+                reset();
+                if (hasRatio()) {
+                    calculateSHTM_VM1_VM2();
+                    double ratioSH = mSH / 100, ratioDV = mDV / 100;
+                    mTienNuoc = ratioSH * calculateTienNuoc_TM_VM1_VM2() + ratioDV * calculateDV();
+                } else {
+                    calculateSHTM_VM();
+                    mTienNuoc = SHTM * 5300 + SHVM1 * 16900;
+                }
+                break;
+            case "16":
             case "26":
+                reset();
+                if (mSH > 0) {
+                    calculateSHTM_VM1_VM2();
+                    double ratioSH = mSH / 100, ratioSX = mSX / 100, ratioDV = mDV / 100;
+                    mTienNuoc = ratioSH * calculateTienNuoc_TM_VM1_VM2() + ratioSX * calculateSX() + ratioDV * calculateDV();
+                } else {
+                    calculateSHTM_VM();
+                    double ratioSX = mSX / 100, ratioDV = mDV / 100;
+                    mTienNuoc = ratioSX * calculateSX() + ratioDV * calculateDV();
+                }
+                break;
+            case "17":
             case "27":
-                //todo không ghi chỉ số
-                mTieuThu = calTieuThuTB();
-                mCSM = mTieuThu + mCSC;
+                mTienNuoc = mTieuThu * 5300;
                 break;
+            case "18":
             case "28":
-                //todo để trống
-                mTieuThu = calTieuThuTB();
-                mCSM = mTieuThu + mCSC;
+            case "38":
+                reset();
+                if (hasRatio()) {
+                    calculateSHTM_VM1_VM2();
+                    double ratioSH = mSH / 100, ratioHC = mHC / 100;
+                    mTienNuoc = ratioSH * calculateTienNuoc_TM_VM1_VM2() + ratioHC * calculateHC();
+                } else {
+                    calculateSHTM_VM();
+                    mTienNuoc = SHTM * 5300 + SHVM1 * 10300;
+                }
                 break;
+            case "19":
             case "29":
-                mTieuThu = calTieuThuTB();
-                mCSM = -1;
+            case "39":
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSH = mSH / 100, ratioSX = mSX / 100, ratioDV = mDV / 100, ratioHC = mHC / 100;
+                mTienNuoc = ratioSH * calculateTienNuoc_TM_VM1_VM2() + ratioSX * calculateSX() + ratioDV * calculateDV() + ratioHC * calculateHC();
+                break;
+            case "21":
+                reset();
+                calculateSHTM_VM1_VM2();
+                mTienNuoc = calculateTienNuoc_TM_VM1_VM2();
                 break;
             case "31":
-//                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-//                    mCSM = Integer.parseInt(mCSMString);
-                mTieuThu = calTieuThuTB();
-                mCSM = mCSC + mTieuThu;
+                mTienNuoc = mTieuThu * 10300;
                 break;
-            case "32":
-                //todo để trống
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - mCSC;
-                }
-//                mTieuThu = calTieuThuTB();
-//                mCSM = mTieuThu + mCSC;
-//                mTieuThu = calTieuThuTB();
-//                mCSM = mCSC + mTieuThu;
-                break;
-            case "33":
             case "34":
-                //todo để trống
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - mCSC;
-                }
-//                mTieuThu = calTieuThuTB();
-//                mCSM = mCSC + mTieuThu;
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSX = mSX / 100, ratioHC = mHC / 100;
+                mTienNuoc = ratioSX * calculateSX() + ratioHC * calculateHC();
                 break;
             case "35":
-                mCSM = mCSC;
-                mTieuThu = calTieuThuTB();
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioDV = mDV / 100, ratioHC = mHC / 100;
+                mTienNuoc = ratioDV * calculateDV() + ratioHC * calculateHC();
                 break;
             case "36":
-            case "38":
-            case "39":
-                mTieuThu = calTieuThuTB();
-                mCSM = mTieuThu + mCSC;
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSX = mSX / 100, ratioDV = mDV / 100, ratioHC = mHC / 100;
+                mTienNuoc = ratioSX * calculateSX() + ratioDV * calculateDV() + ratioHC * calculateHC();
                 break;
             case "41":
-                //todo để trống
-                mCSM = mCSC;
-                mTieuThu = 0;
+                mTienNuoc = mTieuThu * 11400;
                 break;
-            case "42":
-            case "43":
             case "44":
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSX = mSX / 100, ratioSH = mSH / 100;
+                mTienNuoc = ratioSX * calculateSX() + ratioSH * calculateSH();
+                break;
             case "45":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0) {
-                    mCSM = Integer.parseInt(mCSMString);
-                    mTieuThu = mCSM - mCSC;
-                }
-                //todo gắn mới
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioDV = mDV / 100, ratioSH = mSH / 100;
+                mTienNuoc = ratioDV * calculateDV() + ratioSH * calculateSH();
                 break;
             case "46":
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSX = mSX / 100, ratioSH = mSH / 100, ratioDV = mDV / 100;
+                mTienNuoc = ratioSX * calculateSX() + ratioSH * calculateSH() + ratioDV * calculateDV();
+                break;
             case "51":
+                reset();
+                calculateSHTM_VM1_VM2();
+
+                mTienNuoc = SHTM * 4770 + SHVM1 * 9180 + SHVM2 * 10260;
+                break;
             case "52":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-                    mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
-                mTieuThu = 0;
+                mTienNuoc = mTieuThu * 8640;
                 break;
             case "53":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-                    mCSM = Integer.parseInt(mCSMString);
-                mTieuThu = retour();
-                //todo retour
+                mTienNuoc = mTieuThu * 15210;
                 break;
             case "54":
-                //todo
-                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-                    mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
-                mTieuThu = 0;
+                mTienNuoc = mTieuThu * 9270;
                 break;
             case "59":
-                if (!mCSMString.equals("null") && mCSMString.length() > 0)
-                    mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
-                mTieuThu = 0;
+                reset();
+                calculateSHTM_VM1_VM2();
+                double ratioSX = mSX / 100, ratioSH = mSH / 100, ratioDV = mDV / 100, ratioHC = mHC / 100;
+                mTienNuoc = ratioSH * (SHTM * 4770 + SHVM1 * 9180 + SHVM2 * 10260) +
+                        ratioSX * mTieuThu * 8640 + ratioDV * mTieuThu * 15210 + ratioHC * mTieuThu * 9270;
                 break;
             case "68":
+                reset();
+                calculateSHTM_VM1_VM2();
+                double  ratioSH = mSH / 100, ratioDV = mDV / 100;
+                mTienNuoc = ratioSH * (SHTM * 4770 + SHVM1 * 9180 + SHVM2 * 10260) +
+                        + ratioDV * mTieuThu * 15210 ;
                 break;
             default:
+                mTienNuoc = 0;
                 break;
 
         }
 
     }
-
-    private int retour() {
-        int tieuThu;
-        String csmString = "1";
-
-        int lenghtCSC = (mCSC + "").length();
-        int lenghtCSM = (mCSM + "").length();
-        int needAdd = lenghtCSC - lenghtCSM;
-        for (int i = 0; i < needAdd; i++) {
-            csmString += "0";
-        }
-        csmString += (mCSM + "");
-        tieuThu = Integer.parseInt(csmString) - mCSC;
-        return tieuThu;
-    }
-
-    private int calTieuThuTB() {
-        int csc = Integer.parseInt(mCodeCSCSanLuong.getCSC1());
-        String tt1 = mCodeCSCSanLuong.getSanLuong1();
-        String tt2 = mCodeCSCSanLuong.getSanLuong2();
-        String tt3 = mCodeCSCSanLuong.getSanLuong3();
-        int tieuThu1 = 0, tieuThu2 = 0, tieuThu3 = 0;
-        if (tt1.length() > 0)
-            tieuThu1 = Integer.parseInt(tt1);
-        if (tt2.length() > 0)
-            tieuThu2 = Integer.parseInt(tt2);
-        if (tt3.length() > 0)
-            tieuThu3 = Integer.parseInt(tt3);
-        double tempDouble = ((double) (tieuThu1 + tieuThu2 + tieuThu3)) / 3;
-        //lấy 1 chữ số sau dấu thập phân
-        int tempInt = (int) (tempDouble * 10);
-        String s = tempInt + "";
-        int lastNumber = Integer.parseInt(s.substring(s.length() - 1));
-        if (lastNumber <= 5)
-            return tempInt / 10;
-        else return tempInt / 10 + 1;
-    }
-
-    public static boolean checkCSMFluctuation(String tt, String tt1, String tt2, String tt3) {
-        int tieuThu = 0, sum = 0, avergare = 0, min = 0, max = 0;
-        List<Integer> tieuThuList = new ArrayList<>();
-        if (tt.length() > 0) {
-            tieuThu = Integer.parseInt(tt);
-            if (tt1.length() > 0)
-                tieuThuList.add(Integer.parseInt(tt1));
-            if (tt2.length() > 0)
-                tieuThuList.add(Integer.parseInt(tt2));
-            if (tt3.length() > 0)
-                tieuThuList.add(Integer.parseInt(tt3));
-            for (Integer item : tieuThuList)
-                sum += item;
-            avergare = sum / tieuThuList.size();
-            min = avergare / 2;
-            max = 3 * avergare / 2;
-            for (Integer item : tieuThuList)
-                if (max < item)
-                    max = item;
-            if (min <= tieuThu && tieuThu <= max)
-                return false;
-            else
-                return true;
-
-        } else
-            return false;
-    }
-
 
 }
