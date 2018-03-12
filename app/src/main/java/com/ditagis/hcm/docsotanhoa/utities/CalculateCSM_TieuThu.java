bf1825053f9f22807c48a87007b85738106fca51
thanle95 <lengocthan95@gmail.com>
2018-03-12 11:31:44 +0700

    tat cap so nen code 68, N, Q

--- app/src/main/java/com/ditagis/hcm/docsotanhoa/utities/CalculateCSM_TieuThu.java
+++ app/src/main/java/com/ditagis/hcm/docsotanhoa/utities/CalculateCSM_TieuThu.java
@@ -208,8 +208,8 @@ public class CalculateCSM_TieuThu {
             case "N3":
                 if (!mCSMString.equals("null") && mCSMString.length() > 0)
                     mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
+//                else
+//                    mCSM = 0;
                 mTieuThu = 0;
                 break;
             case "X ":
@@ -222,15 +222,15 @@ public class CalculateCSM_TieuThu {
                 //todo
                 if (!mCSMString.equals("null") && mCSMString.length() > 0)
                     mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
+//                else
+//                    mCSM = 0;
                 mTieuThu = 0;
                 break;
             case "Q ":
                 if (!mCSMString.equals("null") && mCSMString.length() > 0)
                     mCSM = Integer.parseInt(mCSMString);
-                else
-                    mCSM = 0;
+//                else
+//                    mCSM = 0;
                 mTieuThu = 0;
                 break;
             default:
