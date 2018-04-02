731575d368b82b1135c1d104d12e006a5f04d386
thanle95 <lengocthan95@gmail.com>
2018-04-02 16:49:03 +0700

    cai dat ngay 02/04

--- app/src/main/java/com/ditagis/hcm/docsotanhoa/LoginActivity.java
+++ app/src/main/java/com/ditagis/hcm/docsotanhoa/LoginActivity.java
@@ -80,8 +80,8 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
-//                loginWithIMEI();
+//                login();
+                loginWithIMEI();
             }
         });
 
